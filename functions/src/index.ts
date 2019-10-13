470b103feb54d5637c8d081b23c6ace23b26211f
Michael Dowden <michael@dowden.us>
2019-10-13 17:58:04 +0200

    cleaned up functions to latest standards

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,3 @@
-//  Firebase Cloud Function Libraries
-import * as admin from 'firebase-admin';
-
-// Import Functions
-import * as Roles from './functions/roles';
-import * as MessageToZapier from './functions/message-to-zapier';
-
-  admin.initializeApp();
-
-//  Export Functions
-export const roles = Roles.listener
-export const messageToZapier = MessageToZapier.listener
+// Export Functions
+export * from './functions/messages-to-zapier'
+export * from './functions/roles'
