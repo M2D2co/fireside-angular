2f5a07ffe0a4766339f1b5e522d3eec0ccfab751
Michael Dowden <michael@andromeda16.com>
2020-10-04 17:18:03 -0400

    Extracted Firebase config to it's own file. Remove Firebase config from Git. Silenced CommonJS warnings. Added fsevents as optional dependency to eliminate install warnings. Removed unused script from npm. Updated page title.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
+import * as firebaseConfig from './firebase-config.json';
+
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAqvBkj8dpEL22TKMe8f2mfaL7MmEOcLcM',
-    authDomain: 'fireside-73c10.firebaseapp.com',
-    databaseURL: 'https://fireside-73c10.firebaseio.com',
-    projectId: 'fireside-73c10',
-    storageBucket: 'fireside-73c10.appspot.com',
-    messagingSenderId: '811594146603',
-    appId: '1:811594146603:web:95ca49e8f7cd3937dd5b6e'
+    apiKey: firebaseConfig.apiKey,
+    authDomain: firebaseConfig.authDomain,
+    databaseURL: firebaseConfig.databaseURL,
+    projectId: firebaseConfig.projectId,
+    storageBucket: firebaseConfig.storageBucket,
+    messagingSenderId: firebaseConfig.messagingSenderId,
+    appId: firebaseConfig.appId,
   },
   google: {
     trackingId: null,
